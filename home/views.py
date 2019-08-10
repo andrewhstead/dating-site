0b651a0e45f9d4ddb0102bd622035230da2d8924
Andrew Stead <andrew@andrewstead.co.uk>
2019-08-10 09:16:39 +0100

    Created basic Home Page template.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# The default home page for the site.
+def home_page(request):
+    page_name = "Home Page"
+
+    return render(request, "home.html", {
+        'page_name': page_name
+    })
