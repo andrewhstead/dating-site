511f10a5baf21eda5d8e379e14875c6d9c3d0e42
Andrew Stead <andrew@andrewstead.co.uk>
2019-08-10 10:49:12 +0100

    Added site navigation menu.

---
+++ static/js/custom.js
@@ -0,0 +1,4 @@
+// Function to allow the user to toggle the navigation menu on and off in the default stylesheet.
+$('#menu-button').click(function(){
+    $('#main-menu').slideToggle(1000);
+});
--- users/models.py
+++ users/models.py
@@ -1,6 +1,30 @@
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
+# Options for ethnicity of user.
+ETHNICITY = (
+    ('black', "Black"),
+    ('white', "White"),
+    ('asian', "Asian"),
+)
+
+# Options for denomination of user.
+DENOMINATION = (
+    ('catholic', "Catholic"),
+    ('anglican', "Anglican"),
+    ('methodist', "Methodist"),
+    ('baptist', "Baptist"),
+)
+
+# Options for diet of user.
+DIET = (
+    ('meat-eater', "Meat Eater"),
+    ('vegetarian', "Vegetarian"),
+    ('pescatarian', "Pescatarian"),
+    ('vegan', "Vegan"),
+    ('marmite', "Marmite Only"),
+)
+
 # Options for gender of user and person being sought.
 GENDER = (
     ('M', "Male"),
@@ -23,12 +47,14 @@ class User(AbstractUser):
     subscription_ends = models.DateTimeField(blank=True, null=True)
     subscription_renews = models.BooleanField(default=False)
     subscription_plan = models.CharField(max_length=25, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    ethnicity = models.CharField(max_length=25, choices=ETHNICITY, blank=True, null=True)
+    denomination = models.CharField(max_length=25, choices=DENOMINATION, blank=True, null=True)
+    diet = models.CharField(max_length=25, choices=DIET, blank=True, null=True)
     gender = models.CharField(max_length=25, choices=GENDER, blank=True, null=True)
     looking_for = models.CharField(max_length=25, choices=GENDER, blank=True, null=True)
     relationship = models.CharField(max_length=25, choices=RELATIONSHIP, blank=True, null=True)
     profile_picture = models.ImageField(upload_to="images/users", blank=True, null=True)
-    address_line_1 = models.CharField(max_length=100, blank=True, null=True)
-    address_line_2 = models.CharField(max_length=100, blank=True, null=True)
     city = models.CharField(max_length=50, blank=True, null=True)
     postcode = models.CharField(max_length=10, blank=True, null=True)
     country = models.CharField(max_length=50, blank=True, null=True)
