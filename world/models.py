b8bff6a48582332fc1f95aac08505b9050e6ef81
Andrew Stead <andrew@andrewstead.co.uk>
2019-08-13 11:02:47 +0100

    Removed short name from country model.

--- world/models.py
+++ world/models.py
@@ -6,13 +6,12 @@ from django.db import models
 # Countries which are available for selection.
 class Country(models.Model):
     objects = models.Manager()
-    short_name = models.CharField(max_length=25, unique=True)
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=50, unique=True)
     abbreviation = models.CharField(max_length=3, unique=True)
     flag = models.ImageField(upload_to="images/countries", blank=True, null=True)
 
     class Meta:
-        ordering = ['short_name']
+        ordering = ['name']
 
     def __str__(self):
         return self.name
