450efdd0dbf2edd8a36186c68619b9cf8025296a
Andrew Stead <andrew@andrewstead.co.uk>
2019-10-18 19:53:03 +0100

    Added models for discussion forum.

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Section, Board, Thread, Post
 
 # Register your models here.
+admin.site.register(Section)
+admin.site.register(Board)
+admin.site.register(Thread)
+admin.site.register(Post)
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,55 @@
+
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
+from tinymce.models import HTMLField
+from users.models import User
+
 
 # Create your models here.
+# A section within the forum - used to categorise the boards.
+class Section(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.title
+
+
+# The individual boards within the forum. Can be linked to a specific team or be general league-wide boards.
+class Board(models.Model):
+    title = models.CharField(max_length=50)
+    section = models.ForeignKey(Section, related_name='boards', on_delete=models.CASCADE)
+    thread_count = models.IntegerField(default=0)
+    post_count = models.IntegerField(default=0)
+    description = HTMLField(blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+
+# A thread in the forum. Attributed to the user who created the thread and allocated to the relevant board. The
+# 'last_post' field will be updated every time a post is added or deleted.
+class Thread(models.Model):
+    title = models.CharField(max_length=50)
+    user = models.ForeignKey(User, related_name='threads', on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, related_name='threads', on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    post_count = models.IntegerField(default=0)
+    last_post = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
+
+
+# An individual forum post, attributed to a user an allocated to the relevant thread.
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    thread = models.ForeignKey(Thread, related_name='posts', on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, related_name='posts', on_delete=models.CASCADE)
+    content = HTMLField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.user
