761352531018d36f705256c50b480a07c1ed3d05
Andrew Stead <andrew@andrewstead.co.uk>
2020-05-06 21:14:28 +0100

    Search now filtering by age with slight inaccuracy to be fixed.

--- search/views.py
+++ search/views.py
@@ -4,9 +4,10 @@ from django.template.context_processors import csrf
 from .forms import SearchForm
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from users.models import User
+from users.models import User, user_age
 from django.http import JsonResponse, HttpResponseRedirect
 from django.db.models import Q
+from datetime import date, timedelta
 
 # Create your views here.
 
@@ -44,13 +45,27 @@ def search_results(request):
         user.last_active = timezone.now()
         user.save()
 
+    today = date.today()
+    users = User.objects.filter(is_staff=False)
+
     country = request.GET.get('country', '')
     gender = request.GET.get('gender', '')
+    ethnicity = request.GET.get('ethnicity', '')
+    age_low = request.GET.get('age_low', '')
+    age_high = request.GET.get('age_high', '')
 
-    results = User.objects.filter(country_id=country)
+    results = users.filter(country_id=country).order_by('-last_active')
 
     if gender:
         results = results.filter(gender=gender)
+    if ethnicity:
+        results = results.filter(ethnicity=ethnicity)
+    if age_low:
+        latest_date = today - timedelta(days=(int(age_low)*365))
+        results = results.filter(date_of_birth__lte=latest_date)
+    if age_high:
+        earliest_date = today - timedelta(days=(int(age_high)*365))
+        results = results.filter(date_of_birth__gte=earliest_date)
 
     args = {
         'page_name': page_name,
